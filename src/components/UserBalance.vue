98f669b9fe5e4148614b5687bef506dac5f716af
Juan <jbocane6@gmail.com>
2020-12-17 08:27:52 -0500

    ultimo

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -17,7 +17,7 @@ export default {
 created: function(){
 	this.username = this.$route.params.username
 	let self = this
-	axios.get("https://cajero---api.herokuapp.com/user/balance/" + this.username)
+	axios.get("https://tracker--api.herokuapp.com/user/balance/" + this.username)
 		.then((result) => {
 			self.balance = result.data.balance
 		})
